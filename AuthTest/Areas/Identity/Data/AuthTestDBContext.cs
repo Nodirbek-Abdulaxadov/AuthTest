da399931df6faa7572bb8dcb46910665a6a3cf47
Nodirbek-Abdulaxadov <nbkabdulaxadov2000@gmail.com>
2021-10-23 04:28:18 +0500

    Add project files.

---
+++ AuthTest/Areas/Identity/Data/AuthTestDBContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthTest.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthTest.Data
+{
+    public class AuthTestDBContext : IdentityDbContext<AuthTestUser>
+    {
+        public AuthTestDBContext(DbContextOptions<AuthTestDBContext> options)
+            : base(options)
+        {
+        }
+        //DbSet<AuthTestUser> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ AuthTest/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using AuthTest.Areas.Identity.Data;
+using AuthTest.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthTest.Areas.Identity.IdentityHostingStartup))]
+namespace AuthTest.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthTestDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthTestDBContextConnection")));
+
+                services.AddDefaultIdentity<AuthTestUser>(options => options.SignIn.RequireConfirmedAccount = false
+                                                            /*{
+                                                             * options.SignIn.RequireConfirmedAccount = false
+                                                             * options.Password.RequiredLowerCase = false
+                                                             * options.Password.RequiredUpperCase = false
+                                                             }*/
+                                                            )
+                    .AddEntityFrameworkStores<AuthTestDBContext>();
+            });
+        }
+    }
+}
